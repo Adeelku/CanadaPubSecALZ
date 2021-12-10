25111d4c2a1b93e83a37c3af4aa5c9d0175bea2d
Adil Touati <adiltouati@microsoft.com>
2021-12-09 23:11:30 -0500

    (#113) serviceConnection_mgmt_groups #2

--- scripts/sp/create-sps.sh
+++ scripts/sp/create-sps.sh
@@ -1,4 +1,8 @@
 # Create SP for management-groups-ci
+# Microsoft.Resources/deployments/operationStatuses/read
+# Microsoft.Resources/deployments/write
+# Microsoft.Resources/deployments/validate/action
+
 az ad sp create-for-rbac \
 -n "spn-azure-platform-ops-mgmt-groups" \
 --scopes "/" \
